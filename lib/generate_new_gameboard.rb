d445cf321e75560f8b5060819adf1a5ae0d17b38
ccorrigan <barnumswinter@gmail.com>
2017-04-01 10:16:23 -0600

    Added #generate_blank_gameboard method and tests

--- lib/generate_new_gameboard.rb
+++ lib/generate_new_gameboard.rb
@@ -1,23 +1,23 @@
-require 'pry'
-
 class GenerateNewGameboard
   attr_reader :size
   def initialize(size)
     @size = size
   end
 
-  # def generate_blank_gameboard(size)
-  #   sequential_row_names = ("a".."z").to_a
-  #   rows = sequential_row_names.slice(0..(size - 1))
-  #   blank_gameboard = rows.map do |row|
-  #     row = (1..size).map do |num|
-  #       "#{row}#{num}"
-  #     end
-  #   end
-  #   blank_gameboard
-  # end
+  def generate_blank_gameboard
+    gameboard_spaces = initialize_empty_gameboard_spaces
+    blank_gameboard = gameboard_spaces.map do |letter_array|
+      row = {} 
+      letter_array.each do |cell_name|
+        row[cell_name] = Cell.new
+      end
+      row
+    end
+    blank_gameboard
+    end
+      
 
-  def initialize_empty_gameboard_spaces(size)
+  def initialize_empty_gameboard_spaces
     sequential_row_names = ("a".."z").to_a
     rows = sequential_row_names.slice(0..(size - 1))
     empty_gameboard_spaces = rows.map do |row|
--- test/generate_new_gameboard_test.rb
+++ test/generate_new_gameboard_test.rb
@@ -16,10 +16,19 @@ class TestGenerateNewGameboard < Minitest::Test
 
   def test_initialize_empty_gameboard_spaces_return_values
     new_gameboard = GenerateNewGameboard.new(4)
-    blank_gameboard = new_gameboard.initialize_empty_gameboard_spaces(4)
+    empty_gameboard = new_gameboard.initialize_empty_gameboard_spaces
+    assert_equal Array, empty_gameboard.class
+    assert_equal 4, empty_gameboard.length
+    assert_equal Array, empty_gameboard.first.class
+    assert_equal 4, empty_gameboard.first.length
+  end
+
+  def test_generate_blank_gameboard_return_values
+    new_gameboard = GenerateNewGameboard.new(4)
+    blank_gameboard = new_gameboard.generate_blank_gameboard
     assert_equal Array, blank_gameboard.class
     assert_equal 4, blank_gameboard.length
-    assert_equal Array, blank_gameboard.first.class
+    assert_equal Hash, blank_gameboard.first.class
     assert_equal 4, blank_gameboard.first.length
   end
 end
