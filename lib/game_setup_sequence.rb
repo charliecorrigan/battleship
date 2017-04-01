c167716dbd75db0386ada5c60a9368ca408f5195
ccorrigan <barnumswinter@gmail.com>
2017-04-01 08:46:03 -0600

    Add #create_computer_gameboard and tests

--- lib/game_setup_sequence.rb
+++ lib/game_setup_sequence.rb
@@ -1,14 +1,24 @@
 class GameSetupSequence
-  attr_reader :difficulty
-
+  attr_reader :board_size
   def initialize(difficulty = "beginner")
-    @difficulty = difficulty
+    @board_size = select_board_size(difficulty)
   end
 
-  # create_computer_gameboard
-  #   generate_new_gameboard
-  #   place_ships(computer)
-  #   update_gameboard
+  def select_board_size(difficulty)
+    size = 4 if difficulty == "beginner"
+    size = 8 if difficulty == "intermediate"
+    size = 12 if difficulty == "advanced"
+    size
+  end
+
+  def create_computer_gameboard
+    new_computer_gameboard = GenerateNewGameboard.new(board_size)
+    new_game_board = new_computer_gameboard.generate_blank_gameboard
+    computer_fleet = GenerateFleet.new
+    ships = computer_fleet.return_ship_coordinates("computer")
+    computer_gameboard = new_computer_gameboard.populate_gameboard_with_ships(new_game_board, ships)
+    computer_gameboard
+  end
 
   # create_player_gameboard
   #   generate_new_gameboard
--- test/game_setup_sequence_test.rb
+++ test/game_setup_sequence_test.rb
@@ -15,17 +15,23 @@ class TestGameSetupSequence < Minitest::Test
 
   def test_it_initializes_with_beginner_as_difficulty_by_default
     new_setup = GameSetupSequence.new
-    assert_equal "beginner", new_setup.difficulty
+    assert_equal 4, new_setup.board_size
   end
 
   def test_difficulty_can_be_specified_and_changed_on_initialization
     new_setup = GameSetupSequence.new("beginner")
-    assert_equal "beginner", new_setup.difficulty
+    assert_equal 4, new_setup.board_size
     new_setup = GameSetupSequence.new("intermediate")
-    assert_equal "intermediate", new_setup.difficulty
+    assert_equal 8, new_setup.board_size
     new_setup = GameSetupSequence.new("advanced")
-    assert_equal "advanced", new_setup.difficulty
+    assert_equal 12, new_setup.board_size
   end
 
-  
+  def test_create_computer_gameboard_return_values
+    new_setup = GameSetupSequence.new("beginner")
+    computer_gameboard = new_setup.create_computer_gameboard
+    assert_equal Array, computer_gameboard.class
+    assert_equal 4, computer_gameboard.length
+    assert_equal Hash, computer_gameboard[0].class
+  end
 end
