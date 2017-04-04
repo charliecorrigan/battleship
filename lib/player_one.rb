b7c634770e71569ff6496d5ab0f5a2fd19e0080d
ccorrigan <barnumswinter@gmail.com>
2017-04-03 19:38:57 -0600

    Add #get_valid_player_input and helper methods

--- lib/player_one.rb
+++ lib/player_one.rb
@@ -1,12 +1,70 @@
+
 class PlayerOne
 
-  def player_takes_a_turn(computer_gameboard, player_one_display_board)
-    #runner_method
-    #player_one_display_board.display
-    #prompt_player_for_guess
-    #display_result
-    #player_one_display_board.display
+  def player_takes_a_turn(computer_gameboard, player_one_display_board) #ships_and_locations
+    winner = false
+    player_one_display_board.display
+    # player_guess = get_valid_player_input(computer_gameboard)
+    # result = calculate_result(player_guess)
+    # display_result(result)
+    # if result = "hit"
+    # if result = "hit"
+    #   sunk = check_is_ship_is_sunk?
+    #     if sunk
+    #       check_if_all_ships_sunk?
+    #     end
+    # end   
+    # player_one_display_board.update
+    # player_one_display_board.display
+    # winner
   end
 
+  def get_valid_player_input(computer_gameboard)
+    valid = false
+    until valid
+      player_guess = solicit_player_guess
+      valid = is_guess_valid?(player_guess, computer_gameboard)
+      valid
+    end
+    player_guess
+  end
 
+  def solicit_player_guess
+    puts "Enter the coordinate you wish to fire upon."
+    player_guess = gets.chomp
+    player_guess
+  end
+
+  def is_guess_valid?(player_guess, computer_gameboard)
+    possible_keys = list_possible_keys(computer_gameboard)
+    split_user_input = player_guess.downcase.split
+    valid_cell = split_user_input.all? do |input|
+      possible_keys.include?(input)
+    end
+    if valid_cell != true
+      puts "Error: That square name doesn't exist."
+    elsif valid_cell && cell(computer_gameboard, player_guess).fired_on
+      puts "Error: That square has already been fired upon."
+      valid_cell = false
+    end
+    valid_cell
+  end
+
+  def cell(computer_gameboard, player_guess)
+    computer_gameboard[(player_guess[0].upcase.ord - 65)][player_guess.downcase]
+  end
+
+  def list_possible_keys(gameboard)
+    possible_keys = []
+    gameboard.each do |row|
+      row.each do |cell|
+        possible_keys << cell[1].name
+      end
+    end
+    possible_keys
+  end
+
+  def calculate_result(player_guess)
+    #<----is cell.ship? change cell status to return "hit"
+  end
 end
--- test/player_one_test.rb
+++ test/player_one_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/player_one'
 require './lib/game_setup_sequence'
+require './lib/display_board'
 
 class TestPlayerOne < Minitest::Test
   def test_it_exists
@@ -12,6 +13,42 @@ class TestPlayerOne < Minitest::Test
   end
 
   def test_player_takes_a_turn
-    #runner_method
+    skip
+    new_setup = GameSetupSequence.new("beginner")
+    computer_gameboard = new_setup.create_computer_gameboard
+    player_one_display_board = DisplayBoard.new(computer_gameboard)
+    player_one = PlayerOne.new
+    player_one.player_takes_a_turn(computer_gameboard, player_one_display_board)
   end
+
+  def test_solicit_player_guess_returns_string
+    skip
+    player_one = PlayerOne.new
+    player_guess = player_one.solicit_player_guess
+    assert_equal String, player_guess.class
+  end
+
+  def test_is_guess_valid
+    skip
+    new_setup = GameSetupSequence.new("beginner")
+    computer_gameboard = new_setup.create_computer_gameboard
+    player_one = PlayerOne.new
+    valid = player_one.is_guess_valid?("A1", computer_gameboard)
+    assert valid
+    valid = player_one.is_guess_valid?("R1", computer_gameboard)
+    refute valid
+    computer_gameboard[0]["a2"].fired_on = true
+    valid = player_one.is_guess_valid?("A2", computer_gameboard)
+    refute valid
+  end
+
+  def test_get_valid_player_input
+    
+    new_setup = GameSetupSequence.new("beginner")
+    computer_gameboard = new_setup.create_computer_gameboard
+    player_one = PlayerOne.new
+    player_guess = player_one.get_valid_player_input(computer_gameboard)
+    assert player_guess
+  end
+
 end
