ace0de2b546c907a05a3b108ac66173a005fbab8
ccorrigan <barnumswinter@gmail.com>
2017-04-04 16:13:14 -0600

    Refactor computer.rb and computer_test.rb, fixed check_fleet bug that reminded player of previously sunk ships every time the computer hits

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -2,7 +2,6 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/cell'
 
-
 class TestCell < Minitest::Test
 
   def test_new_instance_initialize_values
@@ -15,5 +14,15 @@ class TestCell < Minitest::Test
     assert_nil cell.down
     assert_nil cell.left
     assert_nil cell.right
+  end
+
+  def test_attributes_can_be_changed
+    cell = Cell.new("a1")
+    cell.ship = true
+    cell.fired_on = true
+    cell.turn_result = "hit"
+    assert cell.ship
+    assert cell.fired_on
+    assert_equal "hit", cell.turn_result
   end
 end
